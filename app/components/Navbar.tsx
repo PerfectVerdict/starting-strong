6e7d43b2c966af848119c4e5f8f7fe7794d1a23f
Ethan Metz <perfectverdict@protonmail.com>
2025-02-24 09:07:45 -0500

    fonts working

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = async () => {
             <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
             <nav className="flex justify-between items-center">
                 <Link href="/">
-                    <Image src="/logo.png" alt="logo" width={144} height={30} />"
+                    <Image src="/logo.png" alt="logo" width={144} height={30} />
                 </Link>
 
                 <div className="flex items-center gap-5 text-black">
@@ -20,9 +20,13 @@ const Navbar = async () => {
                         <Link href="/startup/create">
                             <span>Create</span>
                         </Link>
-                            {/*<button onClick={signOut}>*/}
-                            {/*    <span>Logout</span>*/}
-                            {/*</button>*/}
+                            <form action={async ()=> {
+                                "use server"
+
+                                await signOut( { redirectTo: "/"})
+                            }} >
+                                <button type="submit">Logout</button>
+                            </form>
 
                             <Link href={`/user/${session}?.id}`}>
                                 <span>{session?.user?.name}</span>
@@ -32,8 +36,7 @@ const Navbar = async () => {
                          "use server"
                          await signIn('github')
                     }}>
-                          <span>Login</span>
-                        <button type="submit">Login</button>
+                          <button type="submit">Login</button>
                       </form>
                 }
                 </div>
